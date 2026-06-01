c46d7faf08e5109ff9b6cd28327ce42dde7ede02
SedricOuambo <ouambosedric@gmail.com>
2026-06-01 17:53:39 -0400

    Merci

--- src/components/connexion/index.tsx
+++ src/components/connexion/index.tsx
@@ -3,9 +3,8 @@ export default function Connexion() {
         <main className="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
             <div className="bg-white p-8 text-slate-800 text-lg rounded-lg shadow-3xl">
                 Page de connexion
-                <p>
-                    Veuillez-vous connecter
-                </p>
+                <p>Veuillez-vous connecter</p>
+                <p>Merci</p>
             </div>
         </main>
     );
