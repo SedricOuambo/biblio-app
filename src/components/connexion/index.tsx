14e6c7c7f07da091729fee48e60804adef84ee72
soultaka19 <117747099+soultaka19@users.noreply.github.com>
2026-06-01 17:53:45 -0400

    toto

--- src/components/connexion/index.tsx
+++ src/components/connexion/index.tsx
@@ -6,6 +6,7 @@ export default function Connexion() {
                 <p>
                     Veuillez-vous connecter
                 </p>
+                <p>ToTo</p>
             </div>
         </main>
     );
